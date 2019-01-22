28f5d74feaf73042e1b1244cb28f165554a0839d
Ichkour <valentin.ichkour@epitch.eu>
2019-01-22 16:21:56 +0100

    add modal, rights, acls

--- server/routes/index.js
+++ server/routes/index.js
@@ -21,9 +21,20 @@ router.get('/repositories', function(req, res) {
             return res.status(200).send({repositories: data, status: 200})
         })
         .catch(e => {
-            return res.status(401).send({response: "Unauthorized", status: 401})
+            return res.status(401).send({response: e, status: 401})
         });
+});
 
+router.get('/acls', function(req, res) {
+    if (req.query.token == null)
+        return res.status(401);
+    const api = new Blih({email: req.query.email, token: req.query.token})
+    api.getACL(req.query.repo)
+        .then(response => {
+            return res.status(200).send({data: response})
+        }).catch(e => {
+            return res.status(401).send({response: e, status: 401})
+    })
 });
 
 module.exports = router;
